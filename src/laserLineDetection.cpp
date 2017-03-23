f2fa9c94465ae427cf8afc8d056e24a032ddda83
Santosh Banisetty <santoshbanisetty@nevada.unr.edu>
2017-03-22 17:51:49 -0700

    Working hallway node

--- src/laserLineDetection.cpp
+++ src/laserLineDetection.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <string>
 #include <stdint.h>
+#include <math.h>
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
@@ -24,6 +25,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <visualization_msgs/Marker.h>
+#include "tf/transform_datatypes.h"
 
 #include "hallway/hallwayMsg.h"
 #include "hallway/markerMsg.h"
@@ -33,6 +35,8 @@
 #define MAXROWS 700
 #define MAXCOLS 700
 #define RESOLUTION_FACTOR 3
+
+double fix = 0;
 
 int laserOriginX = MAXCOLS/2;
 int laserOriginY = MAXROWS/2;
@@ -47,6 +51,7 @@ using namespace cv;
 using namespace std;
 
 string ss;
+float rotation;
 
 ros::Publisher hallwayPublisher;
 ros::Publisher marker_pub;
@@ -55,6 +60,23 @@ ros::Publisher marker_pub;
 void publishHallwayData(vector<float> r, float modeOfTheta);
 void hough_lines(Mat img);
 //void hallwayMarkers(geometry_msgs::Point p1, geometry_msgs::Point p2);
+
+
+
+void robotPositionCallback ( const nav_msgs::Odometry::ConstPtr& msg ){
+ 
+
+ 	//Quaternion to Roll Pitch and Yaw conversion
+  	double roll, pitch, yaw;
+  	tf::Quaternion quat;
+  	tf::quaternionMsgToTF(msg->pose.pose.orientation, quat);
+  	tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);
+	ROS_INFO("Quaternion to RPY %f, %f, %f", roll, pitch, yaw);
+	fix = yaw;
+  
+}
+
+
 
 void laserCallBack(const sensor_msgs::LaserScan::ConstPtr & laserMsg)
 {
@@ -112,20 +134,24 @@ void laserCallBack(const sensor_msgs::LaserScan::ConstPtr & laserMsg)
 		double currentAngle = staringAngle + i*angleIncrement;
 		double r = laserMsg->ranges[i]*100; //range in centemeters
 		//ROS_INFO ("At current angle = %f, the range is %f", currentAngle, r);
-		int x = r * cos(currentAngle);
-		int y = r * sin(currentAngle);
+		int x = r * cos(currentAngle+fix);
+		int y = r * sin(currentAngle+fix);
 		//ROS_INFO ("The X = %d and Y = %d when the origin is at the base laser \n r = %f and current angle = %f ", x, y, r, currentAngle);
 		int shiftedX = abs((x - shiftedOriginX)/RESOLUTION_FACTOR);
 		int shiftedY = abs((y - shiftedOriginY)/RESOLUTION_FACTOR);
 		if ((shiftedX <= MAXCOLS && shiftedX >= 0) && (shiftedY <= MAXROWS && shiftedX >= 0))
 		{
-			/* code */
+			
 			image.at<uchar>(shiftedY, shiftedX) = 0;
 		}
+        /*if ((x <= MAXCOLS && x >= 0) && (y <= MAXROWS && y >= 0))
+        {
+        image.at<uchar>(int(x/RESOLUTION_FACTOR),int(y/RESOLUTION_FACTOR))= 0;
+        }*/
 		
 	} 
 	flip (image, flipped, 0);
-    hough_lines(flipped);
+    hough_lines(image);
     //hough_lines(image);
     // namedWindow("Display Image", WINDOW_AUTOSIZE );
     // imshow("Display Image", image);
@@ -139,7 +165,11 @@ void laserCallBack(const sensor_msgs::LaserScan::ConstPtr & laserMsg)
  	ros::init(argc, argv, "laserLineDetection");
  	ros::NodeHandle n;
  	hallwayPublisher = n.advertise<hallway::hallwayMsg>("hallway_data", 100);
- 	ros::Subscriber laser = n.subscribe("/base_scan", 100, laserCallBack);
+ 	ros::Subscriber laser = n.subscribe("/robot_0/base_scan", 100, laserCallBack);
+    
+    //For Simulator - Subsribe to robot postion
+    ros::Subscriber robotpose_sub = n.subscribe("/robot_0/base_pose_ground_truth", 1000, robotPositionCallback);
+
     marker_pub = n.advertise<hallway::markerMsg>("hallway_marker_points", 100);
   
 
@@ -218,7 +248,9 @@ void laserCallBack(const sensor_msgs::LaserScan::ConstPtr & laserMsg)
 			line( img, pt1, pt2, Scalar(0,0,255), 2, CV_AA);
 		}
     }
-
+    float m = -cos(modeOfTheta)/sin(modeOfTheta);
+    rotation = atan (m);
+    ROS_INFO("slope = %f, angle in radians = %f", m, rotation);
    	publishHallwayData (r, modeOfTheta);
    	//ROS_INFO ("I am here! : %d", debugCount++);
     imshow("Hallway", img);
@@ -253,6 +285,7 @@ void publishHallwayData(vector<float> r, float modeOfTheta)
     std::vector<double> slope;
     std::vector<double> yIntercept;
     //ss << "base_link";
+    std::vector<geometry_msgs::Point> temp;
     std::vector<geometry_msgs::Point> markerPointsL;
     std::vector<geometry_msgs::Point> markerPointsR;
 
@@ -265,19 +298,20 @@ void publishHallwayData(vector<float> r, float modeOfTheta)
     // {
     // 	ROS_INFO ("rvalue [%d] is %f", i, r[i]);
     // }
-    
     if (r.size() == 2) //if hallway 
     {
     	for (int i = 0; i < r.size(); i++)
     	{
-    	//ROS_INFO ("r values [%d] : %f", i, r[i]);
+    	ROS_INFO ("r values [%d] : %f", i, r[i]);
     		if (r[i] > 0)
     		{
 				Point pt1, pt2;
 				double a = cos(modeOfTheta), b = sin(modeOfTheta);
+				//ROS_INFO("===== a and b are : %f, %f", a, b);
 				double x0 = a*r[i], y0 = b*r[i];
 				int backToBaseX = (x0 - laserOriginX)*RESOLUTION_FACTOR;
 				int backToBaseY = (y0 - laserOriginY)*RESOLUTION_FACTOR;
+				//ROS_INFO("======= back to base: %d, %d", backToBaseX, backToBaseY);
 				pt1.x = cvRound(backToBaseX + 1000*(-b));
 				pt1.y = cvRound(backToBaseY + 1000*(a));
 				pt2.x = cvRound(backToBaseX - 1000*(-b));
@@ -289,15 +323,39 @@ void publishHallwayData(vector<float> r, float modeOfTheta)
 					//ROS_INFO ("RAW Point 1 :-: x: %d, y: %d", pt1.x, pt1.y);
 					//ROS_INFO ("RAW Point 2 :-: x: %d, y: %d", pt2.x, pt2.y);
                     //- to eliminate the mirroring of the markers
-                    p1.x = -pt1.x*0.01; // I don't remember why 0.01, Guess m to cm conversion.
+                    /*p1.x = -pt1.x*0.01; // I don't remember why 0.01, Guess m to cm conversion.
                     p1.y = pt1.y*0.01;
                     p1.z = 0;
                     p2.x = -pt2.x*0.01;
                     p2.y = pt2.y*0.01;
+                    p2.z = 0;*/
+
+                    float x1 = -pt1.x*0.01; 
+                    float y1 = pt1.y*0.01;
+                   
+                    float x2 = -pt2.x*0.01;
+                    float y2 = pt2.y*0.01;
+ 
+                    p1.x = x1 * cos(-fix) - y1 * sin(-fix);
+                    p1.y = x1 * sin(-fix) + y1 * cos(-fix);
+                    p1.z = 0;
+                    p2.x = x2 * cos(-fix) - y2 * sin(-fix);
+                    p2.y = x2 * sin(-fix) + y2 * cos(-fix);
                     p2.z = 0;
 
+                    temp.push_back(p1);
+                    temp.push_back(p2);
+
+                    /*p1.x = x1 * cos(fix) - y1 * sin(fix);
+                    p1.y = x1 * sin(fix) * cos(fix) + y1 * pow(cos(fix), 2);
+                    p1.z = 0;
+                    p2.x = x2 * cos(fix) - y2 * sin(fix);
+                    p2.y = x2 * sin(fix) * cos(fix) + y2 * pow(cos(fix), 2);
+                    p2.z = 0;*/
+                    
+
                     //y positive is left side of hallway.
-                    if (p1.y >= 0)
+                    /*if (p1.y <= p2.y)
                     {
                         markerPointsL.push_back(p1);
                         markerPointsL.push_back(p2);
@@ -305,27 +363,38 @@ void publishHallwayData(vector<float> r, float modeOfTheta)
                         ROS_INFO ("Point 1 :-: x: %f, y: %f", p1.x, p1.y);
                         ROS_INFO ("Point 2 :-: x: %f, y: %f", p2.x, p2.y);
                     }
-                    else if (p1.y < 0)
+                    else
                     {
                         markerPointsR.push_back(p1);
                         markerPointsR.push_back(p2);
                         ROS_INFO ("i value : %d", i);
                         ROS_INFO ("Point 1 :-: x: %f, y: %f", p1.x, p1.y);
                         ROS_INFO ("Point 2 :-: x: %f, y: %f", p2.x, p2.y);
-                    }
-                    /*if (i == 0)
-                    {
-                        markerPointsL.push_back(p1);
-                        markerPointsL.push_back(p2);
-                    }
-                    else if (i == 1)
-                    {
-                        markerPointsR.push_back(p1);
-                        markerPointsR.push_back(p2);
                     }*/
+                    if (i == 0 ){
+                    	if( r[i] > r[i+1] ){
+                    		markerPointsL.push_back(p1);
+                        	markerPointsL.push_back(p2);
+                    	}
+                    	else{
+                    		markerPointsR.push_back(p1);
+                        	markerPointsR.push_back(p2);
+                    	}
+                    }
+                    else{
+                    	if( r[i] > r[i-1] ){
+                    		markerPointsL.push_back(p1);
+                        	markerPointsL.push_back(p2);	
+                    	}
+                    	else{
+                    		markerPointsR.push_back(p1);
+                        	markerPointsR.push_back(p2);
+                    	}
+                    }   
                     // hallwayMarkers(p1, p2);
                     if (pt1.y >= 0) //just get the slope of left hallway only
                     {
+                    	ROS_INFO("pt1.y greather than or equal to zero");
                         slope.push_back((float)(pt2.y - pt1.y)/(float)(pt2.x - pt1.x));
                         yIntercept.push_back(pt1.y - slope[i]*pt1.x);// * RESOLUTION_FACTOR);
                         ROS_INFO ("slope: %f and Y-Intercept: %f", slope[i], yIntercept[i]);
@@ -334,34 +403,57 @@ void publishHallwayData(vector<float> r, float modeOfTheta)
 				}
 			}
     	}
+    	for (int i = 0; i < temp.size(); ++i)
+		{
+			/* code */
+			ROS_INFO("%f, %f", temp[i].x, temp[i].y);
+
+		}
+    	
         //Check to see if there is any valid data. if not core dump will occur. 
         //publish hallway data
-    	if (slope.size() > 0 && yIntercept.size() > 0)
-    	{
+    	//if (slope.size() > 0 && yIntercept.size() > 0)
+    	//{
             //ROS_INFO("slope size: %d", slope.size());
     		msg.frame_id = ss;
+            if (slope.size() > 0 && yIntercept.size() > 0)
+            {
+            	ROS_INFO("slope and intercept test");
     		msg.slope_hallwayL = slope[0];
     		msg.intercept_hallwayL = yIntercept[0];
+            }
     		msg.width_hallway = abs(r[0]-r[1]) * RESOLUTION_FACTOR;
             msg.hallwayPointL1 = p1;
             msg.hallwayPointL2 = p2;
 
+            //ROS_INFO("Size of left = %d", markerPointsL.size());
             if (markerPointsL.size() == 2)
             {
+            	ROS_INFO("left markers");
                 markMsg.pointL1 = markerPointsL[0];
                 markMsg.pointL2 = markerPointsL[1];
                 msg.hallwayPointL1 = markerPointsL[0];
                 msg.hallwayPointL2 = markerPointsL[1];
             }
+            else
+            {
+            	//ROS_INFO(":-( %d", markerPointsL.size());
+            }
+            //ROS_INFO("Size of right = %d", markerPointsR.size());
             if (markerPointsR.size() == 2)
             {
+            	ROS_INFO("right markers");
                 markMsg.pointR1 = markerPointsR[0];
                 markMsg.pointR2 = markerPointsR[1];
+            }
+            else
+            {
+            	//ROS_INFO(":-( %d", markerPointsR.size());
             }
             ROS_INFO("frame_id is [%s]", msg.frame_id.c_str());
     		hallwayPublisher.publish(msg);
             marker_pub.publish(markMsg);
-    	}
+    	//}
     	
     }
 
